deb8b44b484a10a0d5033ba2bf199e5a825137a7
taka <d@wakou.cc>
2024-10-31 23:10:04 +0900

    more losses

--- train_and_eval.py
+++ train_and_eval.py
@@ -514,6 +514,7 @@ def run_inductive(
                 out, loss_test_ind, acc_ind, h_list, dist, codebook, loss_list = evaluate(
                     model, data_eval, feats, labels, criterion, evaluator, idx_test_ind
                 )
+            print(f"loss {loss}")
             logger.info(f"Ep {epoch:3d}")
             logger.info(f"loss: {loss.item():.4f}")
             logger.info(f"| s_train: {score_train:.4f}")
