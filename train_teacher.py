79a80dbdd52d0429c1ad1bcfb0e79ac684f5c236
taka <d@wakou.cc>
2024-11-17 23:15:35 +0900

    more losses

--- train_teacher.py
+++ train_teacher.py
@@ -79,7 +79,7 @@ def get_args():
     )
 
     """VQGraph"""
-    parser.add_argument("--codebook_size", type=int, default=100, help="Codebook size of VQGraph")
+    parser.add_argument("--codebook_size", type=int, default=50, help="Codebook size of VQGraph")
     parser.add_argument("--lamb_node", type=float, default=0.001)  # default=0.001)
     parser.add_argument("--lamb_edge",  type=float, default=0.3)  # default=0.03)
 
