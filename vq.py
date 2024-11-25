e8124cd4e238d7a89a7876d0a662ffad09c785ca
taka <d@wakou.cc>
2024-11-25 16:21:02 +0900

    first

--- vq.py
+++ vq.py
@@ -604,9 +604,10 @@ class VectorQuantize(nn.Module):
             x = rearrange(x, f'b n (h d) -> {ein_rhs_eq}', h=heads)
 
         quantize, embed_ind, dist, embed = self._codebook(x)
-        # print(embed)
-        # print(quantize.shape, embed.shape)
-        # print(self.training)
+        print(f"embed {embed}")
+        print(f"embed_ind {embed_ind}")
+        print(f"quantize {quantize}")
+        print(self.training)
         codes = self.get_codes_from_indices(embed_ind)
         # print(codes.shape)
         if self.training:
