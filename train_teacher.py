bfabcf24d02276fff55bdcceee4b610dbe26b126
taka <d@wakou.cc>
2024-11-27 13:30:40 +0900

    first

--- train_teacher.py
+++ train_teacher.py
@@ -105,7 +105,7 @@ def get_args():
     )
 
     """SAGE Specific"""
-    parser.add_argument("--batch_size", type=int, default=5000)
+    parser.add_argument("--batch_size", type=int, default=512)
     parser.add_argument(
         "--fan_out",
         type=str,
