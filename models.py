6ef1314534cbc5b304077dbe95df735c42305387
taka <d@wakou.cc>
2024-11-30 04:40:43 +0900

    first

--- models.py
+++ models.py
@@ -175,8 +175,7 @@ class SAGE(nn.Module):
         h = self.dropout(h)
         h_list.append(h)
         # quantize, embed_ind, loss, dist, self._codebook.embed, raw_commit_loss, x
-        quantized, _, commit_loss, dist, codebook, raw_commit_loss, latents, spread_loss, margin_loss, dist_matrix = self.vq(h)
-        print(dist_matrix)
+        quantized, _, commit_loss, dist, codebook, raw_commit_loss, latents, spread_loss, margin_loss = self.vq(h)
         quantized_edge = self.decoder_1(quantized)
         quantized_node = self.decoder_2(quantized)
         # --------------------
