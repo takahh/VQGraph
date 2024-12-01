85b18809ac654d22febb109171f9e837b246ae2e
taka <d@wakou.cc>
2024-12-01 19:39:24 +0900

    first

--- vq.py
+++ vq.py
@@ -511,7 +511,7 @@ class VectorQuantize(nn.Module):
             margin_weight=0.0001,
             spread_weight=10,
             pair_weight=0.1,
-            orthogonal_reg_active_codes_only=False,
+            orthogonal_reg_active_codes_only=True,
             orthogonal_reg_max_codes=None,
             sample_codebook_temp=0.,
             sync_codebook=False
