dbde87e97a2e01330ef78d9f242004fcfa62c9cc
taka <d@wakou.cc>
2024-12-01 17:56:55 +0900

    first

--- vq.py
+++ vq.py
@@ -673,4 +673,4 @@ class VectorQuantize(nn.Module):
         # if self.training:
         #     print("$$$$$$$   torch.unique(embed_ind).shape[0]")  # this value is 8 at the beginning
             # quantized, _, commit_loss, dist, codebook, raw_commit_loss, latent_vectors
-        return quantize, embed_ind, loss, dist, self._codebook.embed, raw_commit_loss, latents, spread_loss, margin_loss, pair_distance_loss
+        return quantize, embed_ind, loss, dist, self._codebook.embed, raw_commit_loss, latents, margin_loss, pair_distance_loss
