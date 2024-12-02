7eea54cce03f62999b05a69476cf16c775761744
taka <d@wakou.cc>
2024-12-02 10:57:40 +0900

    best codebook confirmed.

--- train_teacher.py
+++ train_teacher.py
@@ -83,7 +83,7 @@ def get_args():
     # --------------
     # VQ
     # --------------
-    parser.add_argument("--codebook_size", type=int, default=30, help="Codebook size of VQGraph")
+    parser.add_argument("--codebook_size", type=int, default=50, help="Codebook size of VQGraph")
     parser.add_argument("--lamb_node", type=float, default=0.008)  # default=0.001)
     parser.add_argument("--lamb_edge",  type=float, default=0.10)  # default=0.03)
 
@@ -125,7 +125,7 @@ def get_args():
     parser.add_argument("--learning_rate", type=float, default=0.0005) # default=0.0001)
     parser.add_argument("--weight_decay", type=float, default=0.0005)
     parser.add_argument(
-        "--max_epoch", type=int, default=200, help="Evaluate once per how many epochs"
+        "--max_epoch", type=int, default=5, help="Evaluate once per how many epochs"
     )
     parser.add_argument(
         "--patience",
