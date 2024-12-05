853b3cf4552a33aec4503b681718f44be363ae23
taka <d@wakou.cc>
2024-12-06 08:36:41 +0900

    best codebook confirmed.

--- Analysis/plot_latents.py
+++ Analysis/plot_latents.py
@@ -60,7 +60,7 @@ def main():
     print(f"plot start...")
     arr_list = []
     for epoch in range(1, 11):
-        if epoch != 10:
+        if epoch != 7:
             continue
         print(f"epoch {epoch}")
         namelist = [f"{path}codebook_{epoch}.npz", f"{path}latent_train_{epoch}.npz"]
