0285b9663728d999e39f486eb0f42ab45fa0336e
taka <d@wakou.cc>
2024-12-08 07:29:42 +0900

    best codebook confirmed.

--- train_and_eval.py
+++ train_and_eval.py
@@ -599,9 +599,9 @@ def run_inductive(
             # --------------------------------
             # check if edge loss is decreasing
             # --------------------------------
-            if loss_list[3].item() < best_score_val:
+            if loss_list[1].item() < best_score_val:
                 best_epoch = epoch
-                best_score_val = loss_list[3].item()
+                best_score_val = loss_list[1].item()
                 state = copy.deepcopy(model.state_dict())
                 cb_at_best = cb_just_trained
                 train_latents_at_best = latent_train
