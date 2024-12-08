a7c37fcb8a1ceef5bc21058e6bc57be1ab17196e
taka <d@wakou.cc>
2024-12-08 14:31:11 +0900

    best codebook confirmed.

--- vq.py
+++ vq.py
@@ -502,7 +502,7 @@ class VectorQuantize(nn.Module):
             decay=0.8,
             eps=1e-5,
             kmeans_init=False,
-            kmeans_iters=20,
+            kmeans_iters=50,
             sync_kmeans=True,
             use_cosine_sim=False,
             threshold_ema_dead_code=0,
