998bd75ac463ecbe0474b7854caccce8edf9a2e4
taka <d@wakou.cc>
2024-12-09 15:27:53 +0900

    best codebook confirmed.

--- train_teacher.py
+++ train_teacher.py
@@ -84,8 +84,8 @@ def get_args():
     # VQ
     # --------------
     parser.add_argument("--codebook_size", type=int, default=500, help="Codebook size of VQGraph")
-    parser.add_argument("--lamb_node", type=float, default=0.0002)  # default=0.001)
-    parser.add_argument("--lamb_edge",  type=float, default=0.03)  # default=0.03)
+    parser.add_argument("--lamb_node", type=float, default=0.001)  # default=0.001)
+    parser.add_argument("--lamb_edge",  type=float, default=0.09)  # default=0.03)
 
     # --------------
     # Model
