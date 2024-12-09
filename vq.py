73976efdefe779e7d30cb84f57e4a65910d835d7
taka <d@wakou.cc>
2024-12-09 14:49:10 +0900

    best codebook confirmed.

--- vq.py
+++ vq.py
@@ -571,7 +571,7 @@ class VectorQuantize(nn.Module):
             threshold_ema_dead_code=0,
             channel_last=True,
             accept_image_fmap=False,
-            commitment_weight=0.0003,
+            commitment_weight=0.0006,
             margin_weight=0.03,
             spread_weight=0.002,
             pair_weight=0.01,
