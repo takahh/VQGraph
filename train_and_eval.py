5900139bf74062b26bfb105673dca752af05a562
taka <d@wakou.cc>
2024-12-10 16:55:33 +0900

    best codebook confirmed.

--- train_and_eval.py
+++ train_and_eval.py
@@ -495,7 +495,7 @@ def run_inductive(
             )
             # save codebook and vectors every epoch
 
-            np.savez(f"./codebook_{epoch}", cb_just_trained.cpu().detach().numpy())
+            np.savez(f"./codebook_{epoch}", cb_just_trained[-1].cpu().detach().numpy())
             # latent_train = torch.cat([torch.squeeze(x) for x in latent_train])
             # random_indices = np.random.choice(latent_train.shape[0], 10000, replace=False)
             # latent_train = latent_train[random_indices]
