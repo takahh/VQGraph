4c995fb5b968eb0383fb8d14271a0434f0a1d1db
taka <d@wakou.cc>
2024-12-10 18:43:31 +0900

    best codebook confirmed.

--- vq.py
+++ vq.py
@@ -735,8 +735,8 @@ class VectorQuantize(nn.Module):
                 # ---------------------------------
                 # linearly combine losses !!!!
                 # ---------------------------------
-                loss = loss + pair_distance_loss * self.pair_weight + self.spread_weight * spread_loss
-                # loss = loss + margin_loss * self.margin_weight + pair_distance_loss * self.pair_weight + self.spread_weight * spread_loss
+                # loss = loss + pair_distance_loss * self.pair_weight + self.spread_weight * spread_loss
+                loss = loss + margin_loss * self.margin_weight + pair_distance_loss * self.pair_weight + self.spread_weight * spread_loss
                 # loss = loss
 
         if is_multiheaded:
