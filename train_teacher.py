aa064ee75f911b03d6a0d37e17bf2c7bfa1e3c2c
taka <d@wakou.cc>
2024-12-12 06:24:18 +0900

    3

--- train_teacher.py
+++ train_teacher.py
@@ -109,7 +109,7 @@ def get_args():
     )
 
     """SAGE Specific"""
-    parser.add_argument("--batch_size", type=int, default=3000)
+    parser.add_argument("--batch_size", type=int, default=5000)
     parser.add_argument(
         "--fan_out",
         type=str,
@@ -122,7 +122,7 @@ def get_args():
 
     """Optimization"""
     parser.add_argument("--accumulation_steps", type=int, default=30) # default=0.0001)
-    parser.add_argument("--learning_rate", type=float, default=0.00005) # default=0.0001)
+    parser.add_argument("--learning_rate", type=float, default=0.0001) # default=0.0001)
     parser.add_argument("--weight_decay", type=float, default=0.0005)
     parser.add_argument(
         "--max_epoch", type=int, default=5, help="Evaluate once per how many epochs"
