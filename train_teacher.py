f2ce634848cc516d1f736a80f95fe5b6876dc750
taka <d@wakou.cc>
2024-12-11 14:16:45 +0900

    best codebook confirmed.

--- train_teacher.py
+++ train_teacher.py
@@ -121,7 +121,7 @@ def get_args():
     )
 
     """Optimization"""
-    parser.add_argument("--accumulation_steps", type=int, default=30) # default=0.0001)
+    parser.add_argument("--accumulation_steps", type=int, default=1) # default=0.0001)
     parser.add_argument("--learning_rate", type=float, default=0.0005) # default=0.0001)
     parser.add_argument("--weight_decay", type=float, default=0.0005)
     parser.add_argument(
