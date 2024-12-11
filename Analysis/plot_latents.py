0942b761277e3b3afead49290f016c31165c4a8b
taka <d@wakou.cc>
2024-12-11 10:53:52 +0900

    best codebook confirmed.

--- Analysis/plot_latents.py
+++ Analysis/plot_latents.py
@@ -58,7 +58,7 @@ def getdata(filename):
 def main():
     print(f"plot start...")
     arr_list = []
-    target = 55
+    target = 24
     for epoch in range(target, target + 1):
         arr = None
         print(f"epoch {epoch}")
@@ -77,7 +77,7 @@ def main():
         arr_combined = np.vstack(arr_list)
         print(f"combined - {arr_combined.shape}")
         # for param in [5, 10, 20, 30, 40, 50]:
-        for param in [1000]:
+        for param in [100]:
             plot_graph(arr_combined, MODE, epoch, param, cb_size)
 
 
