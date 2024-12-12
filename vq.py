dc3c5139d4393a818b3db47e51bdb674af341445
taka <d@wakou.cc>
2024-12-13 06:31:43 +0900

    vq rolled back

--- vq.py
+++ vq.py
@@ -485,7 +485,7 @@ class VectorQuantize(nn.Module):
             decay=0.8,
             eps=1e-5,
             kmeans_init=False,
-            kmeans_iters=30,
+            kmeans_iters=300,
             sync_kmeans=True,
             use_cosine_sim=False,
             threshold_ema_dead_code=0,
