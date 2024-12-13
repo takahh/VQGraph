29160635051172e2de035f89bbf3383bceb26c91
taka <d@wakou.cc>
2024-12-13 12:24:13 +0900

    vq rolled back

--- Analysis/plot_latents.py
+++ Analysis/plot_latents.py
@@ -59,7 +59,7 @@ def getdata(filename):
 def main():
     print(f"plot start...")
     arr_list = []
-    target = 3
+    target = 17
     for epoch in range(target, target + 1):
         arr = None
         print(f"epoch {epoch}")
