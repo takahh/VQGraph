35409c4022ad59980dc4749d0a4558ea79b09581
taka <d@wakou.cc>
2025-01-11 07:10:10 +0900

    vq rolled back

--- train_teacher.py
+++ train_teacher.py
@@ -111,7 +111,7 @@ def get_args():
     )
 
     """SAGE Specific"""
-    parser.add_argument("--batch_size", type=int, default=5000)
+    parser.add_argument("--batch_size", type=int, default=4000)
     parser.add_argument(
         "--fan_out",
         type=str,
