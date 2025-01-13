0ded3f9fe44c5da3108a868f3cfaf59f89a7f943
taka <d@wakou.cc>
2025-01-13 19:59:53 +0900

    vq rolled back

--- models.py
+++ models.py
@@ -258,7 +258,8 @@ class SAGE(nn.Module):
         # --------------------
         # Feat loss
         # --------------------
-        loss = torch.tensor(div_ele_loss.clone().detach(), requires_grad=True)  # Incorrect
+        loss = div_ele_loss
+        # loss = torch.tensor(div_ele_loss.clone().detach(), requires_grad=True)  # Incorrect
         # raw_feat_loss = F.mse_loss(h, quantized_node)
         # feature_rec_loss = self.lamb_node * raw_feat_loss
         # --------------------------
