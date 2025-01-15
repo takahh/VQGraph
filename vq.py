382061bb769d17254f82fcaf01c4b3de82a62f45
taka <d@wakou.cc>
2025-01-15 16:00:35 +0900

    vq rolled back

--- vq.py
+++ vq.py
@@ -988,11 +988,11 @@ class VectorQuantize(nn.Module):
         # ---------------------------------------------------------------
         # atom_type_div_loss = differentiable_codebook_loss(init_feat[:, 0], embed_ind, self.codebook_size)
         atom_type_div_loss = compute_contrastive_loss(latents, embed_ind)
-        print(" &&&&&&&&&&&& atom_type_div_loss  ")
-        print(f"atom_type_div_loss.requires_grad: {atom_type_div_loss.requires_grad}")
-        print(f"atom_type_div_loss.grad_fn: {atom_type_div_loss.grad_fn}")
-        print(f"atom_type_div_loss.shape: {atom_type_div_loss.shape}")
-        print(f"atom_type_div_loss: {atom_type_div_loss}")
+        # print(" &&&&&&&&&&&& atom_type_div_loss  ")
+        # print(f"atom_type_div_loss.requires_grad: {atom_type_div_loss.requires_grad}")
+        # print(f"atom_type_div_loss.grad_fn: {atom_type_div_loss.grad_fn}")
+        # print(f"atom_type_div_loss.shape: {atom_type_div_loss.shape}")
+        # print(f"atom_type_div_loss: {atom_type_div_loss}")
 
         # atom_type_div_loss = feat_elem_divergence_loss(embed_ind, init_feat[:, 0], self.codebook_size)
         # atom_type_div_loss = atom_type_div_loss + compute_contrastive_loss(latents, embed_ind)
