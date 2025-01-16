7abfd50198ffa4b7d0d5b9045b7db6b6368ba81d
taka <d@wakou.cc>
2025-01-16 12:22:19 +0900

    vq rolled back

--- dataloader.py
+++ dataloader.py
@@ -511,7 +511,7 @@ class SparseGraph:
         """Return the (A, X, z) triplet."""
         return self.adj_matrix, self.attr_matrix, self.labels
 
-def load_npz_to_sparse_graph_partial(file_name, percentage=0.1):
+def load_npz_to_sparse_graph(file_name, percentage=0.1):
     """Load a partial SparseGraph from a Numpy binary file.
 
     Parameters
