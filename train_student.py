7f2bbb41c57411edf28cb3299e8a9822725b2b22
taka <d@wakou.cc>
2025-01-16 12:57:42 +0900

    vq rolled back

--- train_student.py
+++ train_student.py
@@ -113,7 +113,7 @@ def get_args():
     )
 
     """Optimization"""
-    parser.add_argument("--percent", type=float, default=0.05)
+    parser.add_argument("--percent", type=float, default=0.1)
     parser.add_argument("--learning_rate", type=float, default=0.01)
     parser.add_argument("--weight_decay", type=float, default=0.0005)
     parser.add_argument(
