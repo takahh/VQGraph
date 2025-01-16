40a5f4b0ccc8f48f178ba8b5cdc564e9bcc50da6
taka <d@wakou.cc>
2025-01-16 13:52:27 +0900

    vq rolled back

--- train_and_eval.py
+++ train_and_eval.py
@@ -511,7 +511,7 @@ def run_inductive(
                 # print("TRAIN STARAT -------------!")
                 # partial sampling, only obs data
                 # this loss is label loss
-                loss, loss_sum_list, latent_train, latents = train_sage(
+                loss, loss_list_list, latent_train, latents = train_sage(
                     model, obs_data, obs_feats, obs_labels, criterion, optimizer, accumulation_steps
                 )
                 model.encoder.reset_kmeans()
@@ -642,7 +642,7 @@ def run_inductive(
             # loss_total = float(loss_list1[0] + loss_list1[1] + loss_list1[2])
             logger.info(f"------------epoch {epoch:3d} -----------------------")
             # logger.info(f"train_known_g, epoch {epoch:3d}, feature_loss: {loss_list[0].item(): 4f}| edge_loss: {loss_list[1].item(): 4f}|"
-            logger.info(f" div_element_loss: {sum(loss_list)/len(loss_list): 4f},|  ")
+            logger.info(f" div_element_loss: {sum(loss_list_list[0])/len(loss_list_list[0]): 4f},|  ")
             # logger.info(f" div_element_loss: {loss_list[0].item(): 4f},| commit_loss: {loss_list[1].item(): 4f}, margin loss {loss_list[4].item(): 4f}, "
             #             f"spread loss {loss_list[3].item(): 4f}, pair loss {loss_list[4].item(): 4f}, "
             #             f"bond_num_div_loss: {loss_list[5].item(): 4f}, aroma_div_loss: {loss_list[6].item(): 4f}, "
@@ -654,8 +654,11 @@ def run_inductive(
 
             print(f"------------epoch {epoch:3d} -----------------------")  # raw_feat_loss, raw_edge_rec_loss, raw_commit_loss, margin_loss, spread_loss, pair_los
             # print(f" div_element_loss: {sum(loss_list)/len(loss_list): 4f},|  ")
-            print(f"train_known_g, div_element_loss: {loss_list[0]: 4f}, bond_num_div_loss: {loss_list[5]: 4f}, aroma_div_loss: {loss_list[6]: 4f}, "
-                  f"ringy_div_loss: {loss_list[7]: 4f}, h_num_div_loss: {loss_list[8]: 4f}")
+            print(f"train_known_g, div_element_loss: {sum(loss_list_list[0])/len(loss_list_list[0]): 4f}, "
+                  f"bond_num_div_loss: {sum(loss_list_list[5])/len(loss_list_list[5]): 4f},"
+                  f" aroma_div_loss: {sum(loss_list_list[6])/len(loss_list_list[6]): 4f}, "
+                  f"ringy_div_loss: {sum(loss_list_list[7])/len(loss_list_list[7]): 4f}, "
+                  f"h_num_div_loss: {sum(loss_list_list[8])/len(loss_list_list[8]): 4f}")
                   # f"ringy_div_loss: {loss_list[7]: 4f}, h_num_div_loss: {loss_list[8]: 4f}, sil_loss: {loss_list[9].item(): 4f}")
             # print(f"test_known_g, div_element_loss: {loss_list0[0].item(): 4f}, | commit_loss: {loss_list0[1].item(): 4f}")
             # print(f"test_unknown_g, "
