e8329d4924593d5b4973520fd8d436786d5ecbea
taka <d@wakou.cc>
2025-01-16 18:58:00 +0900

    vq rolled back

--- vq.py
+++ vq.py
@@ -999,7 +999,8 @@ class VectorQuantize(nn.Module):
         # ---------------------------------------------------------------
         # atom_type_div_loss = differentiable_codebook_loss(init_feat[:, 0], embed_ind, self.codebook_size)
         atom_type_div_loss = compute_contrastive_loss(quantized, init_feat[:, 0], self.codebook_size)
-        print(atom_type_div_loss)
+        print(f"quantized: {quantized[0]}")
+        print(f"init_feat: {init_feat[0]}")
         # print(" &&&&&&&&&&&& atom_type_div_loss  ")
         # print(f"atom_type_div_loss.requires_grad: {atom_type_div_loss.requires_grad}")
         # print(f"atom_type_div_loss.grad_fn: {atom_type_div_loss.grad_fn}")
