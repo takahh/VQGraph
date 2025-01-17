8ed6868acca5b6319bac2f08a0974d3f4bd1ba4c
taka <d@wakou.cc>
2025-01-17 12:12:25 +0900

    vq rolled back

--- train_and_eval.py
+++ train_and_eval.py
@@ -47,6 +47,8 @@ def train_sage(model, dataloader, feats, labels, criterion, optimizer, accumulat
     for step, (input_nodes, output_nodes, blocks) in enumerate(dataloader):
         blocks = [blk.int().to(device) for blk in blocks]
         batch_feats = feats[input_nodes]
+        print(f"input_nodes {input_nodes}")
+        print(f"batch_feats {batch_feats}")
         with torch.cuda.amp.autocast():
             _, logits, loss, _, cb, loss_list3, latent_train, quantized, latents = model(blocks, batch_feats)
             loss = loss * lamb / accumulation_steps
