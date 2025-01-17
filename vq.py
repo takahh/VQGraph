4ed116c88d70a8e1a49443e117bbb1d4a4a01059
taka <d@wakou.cc>
2025-01-17 12:54:25 +0900

    vq rolled back

--- vq.py
+++ vq.py
@@ -340,7 +340,8 @@ def compute_contrastive_loss(z, atom_types, margin=1.0):
     negative_loss = (1.0 - same_type_mask) * torch.clamp(margin - pairwise_distances,
                                                          min=0.0) ** 2  # Push apart different types
     # Combine and return mean loss
-    return (negative_loss).mean()/1000000
+    # return (negative_loss).mean()/1000000
+    return (negative_loss).mean()
     # return (positive_loss + negative_loss).mean()/1000000
 
 
