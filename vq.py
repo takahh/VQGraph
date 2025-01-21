1bb1a1b52cf91d01cf744c5e2ac0ae02d622a6a9
taka <d@wakou.cc>
2025-01-21 14:10:29 +0900

    vq rolled back

--- vq.py
+++ vq.py
@@ -354,8 +354,8 @@ def compute_contrastive_loss(z, atom_types, margin=1.0):
                                                          min=0.0) ** 2  # Push apart different types
     # Combine and return mean loss
     # return (negative_loss).mean()/1000000
-    print((negative_loss).mean())
-    return (negative_loss).mean()/1000000
+    # print((negative_loss).mean())
+    return (negative_loss).mean() * 1000
     # return (positive_loss + negative_loss).mean()/1000000
 
 
