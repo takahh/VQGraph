82064b0d3676552fb20d542b621c3a1a463763d0
taka <d@wakou.cc>
2025-01-25 04:05:53 +0900

    vq rolled back

--- vq.py
+++ vq.py
@@ -957,7 +957,14 @@ class VectorQuantize(nn.Module):
 
         # atom_type_div_loss = feat_elem_divergence_loss(embed_ind, init_feat[:, 0], self.codebook_size)
         # atom_type_div_loss = atom_type_div_loss + compute_contrastive_loss(latents, embed_ind)
+
+        print("------ quantized -------")
+        print(f"quantized.requires_grad: {quantized.requires_grad}")
+        print(f"quantized.grad_fn: {quantized.grad_fn}")
         atom_type_div_loss = compute_contrastive_loss(quantized, init_feat[:, 0])
+        print("------ atom_type_div_loss　-------")
+        print(f"atom_type_div_loss.requires_grad: {atom_type_div_loss.requires_grad}")
+        print(f"atom_type_div_loss.grad_fn: {atom_type_div_loss.grad_fn}")
         bond_num_div_loss = compute_contrastive_loss(quantized, init_feat[:, 1])
         aroma_div_loss = compute_contrastive_loss(quantized, init_feat[:, 4])
         ringy_div_loss = compute_contrastive_loss(quantized, init_feat[:, 5])
