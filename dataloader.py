f7514ac07efe5c8c3a6c989af88d4801596e922a
taka <d@wakou.cc>
2025-01-31 19:22:35 +0900

    aaa

--- dataloader.py
+++ dataloader.py
@@ -533,6 +533,8 @@ def load_npz_to_sparse_graph(file_name, percentage=0.05):
         cutoff = int(num_nodes * percentage)
         # print(f"original nodes: {int(num_nodes)}")
         # print(f"reduced nodes: {cutoff}")
+        np.set_printoptions(threshold=np.inf)
+
         print(f"loader[adj_data] {loader['adj_data']} ********************")
         adj_matrix = sp.csr_matrix(
             (loader["adj_data"], loader["adj_indices"], loader["adj_indptr"]),
