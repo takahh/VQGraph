394f6d3178e884cf1c7b6090ef335d82556ab401
taka <d@wakou.cc>
2025-02-03 12:26:02 +0900

    aaa

--- models.py
+++ models.py
@@ -273,8 +273,10 @@ class SAGE(nn.Module):
         if epoch == 0:
             sample_feat = h.clone().detach()
             adj_sample = g.adjacency_matrix().to_dense()
-            print("adj_matrix in Train")
-            print(adj_sample)
+            import sys
+            sys.stdout.write("Adjacency matrix in Train:\n")
+            sys.stdout.write(str(adj_sample) + "\n")
+            sys.stdout.flush()
 
         # Add edges along with bond order features if available.
         if remapped_bond_orders:
