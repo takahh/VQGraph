a255ecbdb480ef78cedff38e2203ad27a3d1a4ac
taka <d@wakou.cc>
2025-02-04 13:38:59 +0900

    aaa

--- train_and_eval.py
+++ train_and_eval.py
@@ -685,7 +685,8 @@ def run_inductive(
             print(sample_list1[4])
             converted_list = [[[t1.cpu().numpy()], [t2.cpu().numpy()]] for t1, t2 in sample_list1[4]]
             print("converted_list")
-            print(converted_list[:10, :6])
+            print(converted_list.shape)
+            print(converted_list)
             np.savez(f"./sample_bond_to_edge_{epoch}.npz", *converted_list)
 
         if conf["train_or_infer"] == "train":
