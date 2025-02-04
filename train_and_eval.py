7df7dbf3f38cc2d4fe2d5d3ec5b64a18ab1fb6c5
taka <d@wakou.cc>
2025-02-04 12:51:56 +0900

    aaa

--- train_and_eval.py
+++ train_and_eval.py
@@ -682,7 +682,7 @@ def run_inductive(
             np.savez(f"./sample_node_feat_{epoch}", sample_list1[1].cpu())
             np.savez(f"./sample_adj_{epoch}", sample_list1[2].cpu())
             np.savez(f"./sample_bond_order_{epoch}", sample_list1[3].cpu())
-            converted_list = [(t1.cpu().numpy(), t2.cpu().numpy()) for t1, t2 in sample_list1[4]]
+            converted_list = [[t1.cpu().numpy(), t2.cpu().numpy()] for t1, t2 in sample_list1[4]]
             print("converted_list")
             print(converted_list)
             np.savez(f"./sample_bond_to_edge_{epoch}.npz", *converted_list)
