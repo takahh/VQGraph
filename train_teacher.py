b9273094165e01f55aaf7ceb6a1adb89f254601d
taka <d@wakou.cc>
2025-02-08 07:14:12 +0900

    aaa

--- train_teacher.py
+++ train_teacher.py
@@ -247,7 +247,7 @@ def run(args):
     # # logger.info(f"Total {g.number_of_edges()} edges.")
 
     # feats = g.ndata["feat"]
-    # args.feat_dim = g.ndata["feat"].shape[1]
+    args.feat_dim = g.ndata["feat"].shape[1]
     # args.feat_dim = args.hidden_dim
     # args.label_dim = labels.int().max().item() + 1
 
