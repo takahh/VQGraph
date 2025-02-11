0a204460bdf5f02f67f27e049fcbb98f6a1a93df
taka <d@wakou.cc>
2025-02-12 08:35:02 +0900

    loss

--- models.py
+++ models.py
@@ -30,9 +30,9 @@ class WeightedThreeHopGCN(nn.Module):
         self.vq._codebook.reset_kmeans()
 
     def forward(self, batched_graph, features, epoch):
-        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        batched_graph = batched_graph.to(device)
-        features = transform_node_feats(features)
+        # device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        # batched_graph = batched_graph.to(device)
+        # features = transform_node_feats(features)
         h = features.clone()
         init_feat = h.clone()  # Store initial features (for later use)
         edge_type = "_E"  # Batched heterogeneous graph edge type
