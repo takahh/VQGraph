7e0aaa20e639c020047fa2fab3250dd511e0f7b2
taka <d@wakou.cc>
2025-02-12 23:35:27 +0900

    とりあえずうごいた

--- vq.py
+++ vq.py
@@ -1240,6 +1240,7 @@ class VectorQuantize(nn.Module):
         (margin_loss, spread_loss, pair_distance_loss, div_ele_loss, bond_num_div_loss, aroma_div_loss, ringy_div_loss,
           h_num_div_loss, silh_loss, embed_ind, charge_div_loss, elec_state_div_loss) = self.orthogonal_loss_fn(embed_ind, codebook, init_feat, latents, quantize)
         # margin_loss, spread_loss = orthogonal_loss_fn(codebook)
+        print(f"in vq div_ele_loss {div_ele_loss}")
         embed_ind = embed_ind.reshape(embed_ind.shape[-1], 1)
         if embed_ind.ndim == 2:
             # print("embed_ind.ndim == 2")
