03038e5d00788b42dca704477fdf95f49d8a7377
taka <d@wakou.cc>
2025-02-12 12:52:52 +0900

    とりあえずうごいた

--- vq.py
+++ vq.py
@@ -653,7 +653,7 @@ class EuclideanCodebook(nn.Module):
         # ----------------------------------------------------
         # print(f"run kmeans init")
         self.init_embed_(flatten)
-        print(f" kmean init done")
+        # print(f" kmean init done")
         embed = self.embed
         init_cb = self.embed.detach().clone().contiguous()
         dist = -torch.cdist(flatten, embed, p=2)
