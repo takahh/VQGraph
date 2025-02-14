792eb5094d3e6a4861c2d4044ad041bec443eddb
taka <d@wakou.cc>
2025-02-15 00:57:45 +0900

    とりあえずうごいた

--- new_train_and_eval.py
+++ new_train_and_eval.py
@@ -264,7 +264,7 @@ def run_inductive(
         if conf["train_or_infer"] == "train":
             # Iterate through batches
             for idx, (adj_batch, attr_batch) in enumerate(dataloader):
-                if idx == 1:
+                if idx == 5:
                     break
                 glist = convert_to_dgl(adj_batch, attr_batch)  # 10000 molecules per glist
                 chunk_size = 500  # in 10,000 molecules
